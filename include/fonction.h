4852baf1539db1d5c1ce90ef5c3b6d66ca4d385a
Ilan Trigueiro Legrand <ilan.trigueiro-legrand@edu.ecole-89.com>
2024-01-16 15:44:08 +0100

    .h added

---
+++ include/fonction.h
@@ -0,0 +1,14 @@
+/*
+ * E89 Pedagogical & Technical Lab
+ * project:     maze
+ * created on:  2024-01-16 - 11:50 +0100
+ * 1st author:  ilan.trigueiro-legrand - ilan.trigueiro-legrand
+ * description: include fonction
+ */
+
+#ifndef FONCTION_
+#define FONCTION_
+
+int print_base10(int nb);
+
+#endif
