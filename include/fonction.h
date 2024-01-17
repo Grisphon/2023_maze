c9808fe80c401ce73a87f2c387c59cb17ecb6def
Quentin <quentin.mettier@edu.ecole-89.com>
2024-01-17 11:03:28 +0100

    new main

---
+++ include/fonction.h
@@ -0,0 +1,17 @@
+/*
+ * E89 Pedagogical & Technical Lab
+ * project:     maze
+ * created on:  2024-01-16 - 11:50 +0100
+ * 1st author:  ilan.trigueiro-legrand - ilan.trigueiro-legrand
+ * description: include fonction
+ */
+
+#ifndef FONCTION_
+#define FONCTION_
+
+int print_base10(int nb);
+void map_size(char *buffer);
+char *reader(int fd, char **av);
+unsigned int stu_strlen(const char *str);
+
+#endif
