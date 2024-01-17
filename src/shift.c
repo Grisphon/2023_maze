50026b5edc68e0d5d8fe11e3215e929deb58f9a6
Quentin <quentin.mettier@edu.ecole-89.com>
2024-01-17 14:40:21 +0100

    function shorten

--- src/shift.c
+++ src/shift.c
@@ -1,51 +1,70 @@
 #include "fonction.h"
 
+static int north(char *maze, enum facing dir, int pos, int length, int height)
+{
+    if (maze[pos - 1] == '#') {
+        dir = EAST;
+        return shift(maze, dir, pos, length, height);
+    } else {
+        dir = WEST;
+        maze[pos] = ' ';
+        pos = pos - 1;
+        return shift(maze, dir, pos, length, height);
+    }
+}
+
+static int east(char *maze, enum facing dir, int pos, int length, int height)
+{
+    if (maze[pos - length - 1] == '#') {
+        dir = SOUTH;
+        return shift(maze, dir, pos, length, height);
+    } else {
+        dir = NORTH;
+        maze[pos] = ' ';
+        pos = pos - length - 1;
+        return shift(maze, dir, pos, length, height);
+    }
+}
+
+static int south(char *maze, enum facing dir, int pos, int length, int height)
+{
+    if (maze[pos + 1] == '#') {
+        dir = WEST;
+        return shift(maze, dir, pos, length, height);
+    } else {
+        dir = EAST;
+        maze[pos] = ' ';
+        pos = pos + 1;
+        return shift(maze, dir, pos, length, height);
+    }
+}
+
+static int west(char *maze, enum facing dir, int pos, int length, int height)
+{
+    if (maze[pos + length + 1] == '#') {
+        dir = NORTH;
+        return shift(maze, dir, pos, length, height);
+    } else {
+        dir = SOUTH;
+        maze[pos] = ' ';
+        pos = pos + length + 1;
+        return shift(maze, dir, pos, length, height);
+    }
+}
+
 int shift(char *maze, enum facing dir, int pos, int length, int height)
 {
     if (pos >= 0 && pos < length * height) {
         if (maze[pos] == 'G')
             return 0;
-        if (dir == NORTH) {
-            if (maze[pos - 1] == '#') {
-                dir = EAST;
-                return shift(maze, dir, pos, length, height);
-            } else {
-                dir = WEST;
-                maze[pos] = ' ';
-                pos = pos - 1;
-                return shift(maze, dir, pos, length, height);
-            }
-        } else if (dir == EAST) {
-            if (maze[pos - length - 1] == '#') {
-                dir = SOUTH;
-                return shift(maze, dir, pos, length, height);
-            } else {
-                dir = NORTH;
-                maze[pos] = ' ';
-                pos = pos - length - 1;
-                return shift(maze, dir, pos, length, height);
-            }
-        } else if (dir == SOUTH) {
-            if (maze[pos + 1] == '#') {
-                dir = WEST;
-                return shift(maze, dir, pos, length, height);
-            } else {
-                dir = EAST;
-                maze[pos] = ' ';
-                pos = pos + 1;
-                return shift(maze, dir, pos, length, height);
-            }
-        } else {
-            if (maze[pos + length + 1] == '#') {
-                dir = NORTH;
-                return shift(maze, dir, pos, length, height);
-            } else {
-                dir = SOUTH;
-                maze[pos] = ' ';
-                pos = pos + length + 1;
-                return shift(maze, dir, pos, length, height);
-            }
-        }
+        if (dir == NORTH)
+            return north(maze, dir, pos, length, height);
+        else if (dir == EAST)
+            return east(maze, dir, pos, length, height);
+        else if (dir == SOUTH)
+            return south(maze, dir, pos, length, height);
+        else
+            return west(maze, dir, pos, length, height);
     }
     return -1;
 }
